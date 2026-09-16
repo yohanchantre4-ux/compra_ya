3e35299c7ba5a824e1cea07e6d42ff2d592bd302
yohan chantre <yohan@gmail.com>
2026-09-16 06:55:02 -0500

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class CompraYaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CompraYA',
+      title: 'Compra YA',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
